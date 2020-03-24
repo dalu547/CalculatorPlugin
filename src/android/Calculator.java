593bcb1a9c1f2da1ff0b34e40bbef3e01cfb54a8
dalayya <dalayya.kalla@senecaglobal.com>
2020-03-24 14:54:19 +0530

    added uuid method.

--- src/android/Calculator.java
+++ src/android/Calculator.java
@@ -2,6 +2,9 @@ package cordova.plugin.calculator;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+import android.provider.Settings;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,11 +15,17 @@ import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Calculator extends CordovaPlugin 
 {
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
@@ -134,15 +143,20 @@ public class Calculator extends CordovaPlugin
            {
                 int p1 =  Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                // callbackContext.success(""+ (p1/p2));
-
+                
                 Log.d("PluginTest","Hello, Iam from plugin");
                 
-
-                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                
+                CameraManager cameraManager = (CameraManager)this.cordova.getActivity().getSystemService(Context.CAMERA_SERVICE);
                 String cameraId = cameraManager.getCameraIdList()[0];
                 cameraManager.setTorchMode(cameraId, true);
-
+                
+                
+                String uuid = Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+                
+                String model = android.os.Build.MODEL;
+                callbackContext.success("Uuid: "+uuid+" model: "+model);
+                
            } catch (Exception e) {
                e.printStackTrace();
             }
