2b72c3c54c640bfae906b83a79cfa1438298795a
dalayya <dalayya.kalla@senecaglobal.com>
2020-04-12 19:26:24 +0530

    added kotlin supportr

--- src/android/TestActivity.kt
+++ src/android/TestActivity.kt
@@ -1,6 +1,7 @@
-package com.amazonaws.services.chime.sdkdemo
+package cordova.plugin.calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle
 import android.widget.Toast
 
@@ -8,7 +9,7 @@ class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         setContentView(R.layout.activity_test)
 
         Toast.makeText(this,"TestActivity",Toast.LENGTH_SHORT).show();
